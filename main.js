f2cc70a350b4f7a122d0a7762f8df228bc9b8444
向山　翔 <hms0175km@gmail.com>
2021-12-25 17:31:43 +0900

    [Add]プログラム完成

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const app = new Vue({
     el: "#app",
     data: {
         num1: 0,
-        num2: 0
-    }
+        num2: 0,
+        answer:0,
+    },
+    methods: {
+        calc: function() {
+            this.answer = this.num1 + this.num2;
+        }
+    },
 })
